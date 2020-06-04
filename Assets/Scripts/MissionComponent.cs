5cee8ebbb9fc61e4371e4572b0b90cdec5ec8595
HongWonSup <ghd1876@gmail.com>
2020-06-04 23:52:13 +0900

    Make mission list and category

---
+++ Assets/Scripts/MissionComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionComponent : MonoBehaviour
+{
+    public Image msCategory;
+    public Text msName;
+    public Text msPrice;
+}
---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionManager : MonoBehaviour
+{
+    public GameObject content;
+    public GameObject missionContent;
+    public Text categoryLabel;
+
+    public void Start()
+    {
+        ChangeList();
+    }
+
+    public void ChangeList()
+    {
+        // 미션 리스트 초기화
+        Transform[] childList = content.GetComponentsInChildren<Transform>();
+        if (childList != null)
+        {
+            for (int i = 1; i < childList.Length; i++)
+            {
+                if (childList[i] != transform)
+                    Destroy(childList[i].gameObject);
+            }
+        }
+
+        string category = categoryLabel.text;
+
+        foreach(MissionList.Mission mission in MissionList.missions)
+        {
+            if(mission.category == category)
+            {
+                GameObject rightMission = Instantiate(missionContent);
+                rightMission.GetComponent<MissionComponent>().msName.text = mission.name;
+                rightMission.GetComponent<MissionComponent>().msPrice.text = mission.price + "";
+                rightMission.transform.parent = content.transform;
+                rightMission.transform.localScale = missionContent.transform.localScale;
+            }
+        }
+    }
+}
